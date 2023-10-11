1524596f5244c0fc9f89958874a6954c01c99b34
Quentin <q.estrach-ext@betclicgroup.com>
2023-10-11 17:19:57 +0200

    fix: update CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
+interface Cat {
+  name: string;
+  greed: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'betclic-actions';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  function IsAPalindrome(): string {
-    return 132;
+  title = 'betclic-actions';
+  cats: Cat[] = [];
+
+  ngOnInit() {
+    this.getCats();
+    console.log(this.cats);
+  }
+
+  public getCats() {
+    return this.http
+      .get<Cat[]>('https://betclic-test1.azurewebsites.net/api/cats')
+      .subscribe((data) => (this.cats = data));
   }
 }
